0e6b7c8d46c1f29097947bb32750a3f046f1375a
Durgesh <durgeshnidadavolu@gmail.com>
2026-05-16 18:00:13 +0800

    created faqs.js data file

---
+++ src/data/faq.js
@@ -0,0 +1,23 @@
+
+export const faqs = [
+    {
+        id: 1,
+        question: "What is Frontend Mentor, and how will it help me?",
+        answer: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
+    },
+    {
+        id: 2,
+        question: "Is Frontend Mentor free?",
+        answer: "Yes, Frontend Mentor offers free challenges. There is also a premium plan for users who want access to extra resources and design files."
+    },
+    {
+        id: 3,
+        question: "Can I use Frontend Mentor projects in my portfolio?",
+        answer: "Yes, completed projects can be added to your portfolio to showcase your frontend skills to recruiters and clients."
+    },
+    {
+        id: 4,
+        question: "How can I get help if I'm stuck on a challenge?",
+        answer: "You can ask for help from the Frontend Mentor community and review other developers' solutions for guidance."
+    }
+]
