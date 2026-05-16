08a1419c302783b5d4eb3e89e3b9c0b02b759fbe
Durgesh <durgeshnidadavolu@gmail.com>
2026-05-16 19:27:45 +0800

    updated faqItem component

--- src/components/FAQItem.jsx
+++ src/components/FAQItem.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-
+import { useState } from "react";
 const FAQItem = ({question, answer}) => {
+ const [isOpen, setIsOpen] = useState(false);
+
+ const onQuestionHandler = () => {
+    setIsOpen((prev) => !prev)
+ }
+
   return (
-    <div className="faq-item">
-      <div className="faq-question">
+    <div className={`faq-item ${isOpen ? 'is-open' : ''}`}>
+      <div className="faq-question" onClick={onQuestionHandler}>
         <h2>{question}</h2>
         <span className="icon"></span>
-      </div>      
+      </div>
       <div className="faq-answer">
         <p>
           {answer}
