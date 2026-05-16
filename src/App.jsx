12bf8554711442c9a9fcca1f3d30490d5dfcd550
Durgesh <durgeshnidadavolu@gmail.com>
2026-05-16 19:04:57 +0800

    Updated the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
+import './App.css';
+import { faqs } from './data/faq';
+import FAQCard from './components/FAQCard';
+import FAQItem from './components/FAQItem';
+
 const App = () => {
   return (
     <>
       <main className="page">
         <h1>Accordion App</h1>
+          <FAQCard>
+              {faqs.map(faq =>
+                <FAQItem key={faq.id} question={faq.question} answer={faq.answer} />
+              )}
+          </FAQCard>
       </main>
     </>
   )
